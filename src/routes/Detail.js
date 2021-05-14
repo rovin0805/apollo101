3d48cd24725f6cb5e3e49bf3f44ac93f651e5ed3
rovin0805 <qhdrn67@naver.com>
2021-05-14 17:46:20 +0900

    Connecting Detail and Home

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,11 +6,13 @@ import styled from "styled-components";
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
+      id
       title
       medium_cover_image
       description_intro
       language
       rating
+      isLiked @client
     }
     suggestions(id: $id) {
       id
@@ -66,7 +68,11 @@ const Detail = () => {
   return (
     <Container>
       <Column>
-        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        <Title>
+          {loading
+            ? "Loading..."
+            : `${data.movie.title} ${data.movie.isLiked ? "💖" : "😞"}`}
+        </Title>
         <Subtitle>
           {data?.movie?.language} · {data?.movie?.rating}
         </Subtitle>
