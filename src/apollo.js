6a7834b49b55366c1f91db429d082197d40d4dc6
rovin0805 <qhdrn67@naver.com>
2021-05-14 17:34:42 +0900

    local state

--- src/apollo.js
+++ src/apollo.js
@@ -2,6 +2,21 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   uri: " https://movieql2.vercel.app/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+          },
+        });
+      },
+    },
+  },
 });
 
 export default client;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
-  height: 380px;
+  height: 400px;
+  border-radius: 7px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
+  background-color: transparent;
 `;
 
 const Poster = styled.div`
@@ -16,14 +24,24 @@ const Poster = styled.div`
   width: 100%;
   background-size: cover;
   background-position: center center;
+  border-radius: 7px;
 `;
 
-const Movie = ({ id, bg }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg} />
-    </Link>
-  </Container>
-);
+const Movie = ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>
+        {isLiked ? "Unlike" : "Like"}
+      </button>
+    </Container>
+  );
+};
 
 export default Movie;
